7a785da02ca1811287bb55889d818d0c0bdba554
Maksim Kozka <kozkamaksim308@gmail.com>
2026-02-27 21:20:18 +0200

    fix scrol / fix modal-close-btn

--- js/model.js
+++ js/model.js
@@ -1,7 +1,7 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
-  
+
     closeModalBtn: document.querySelector("[data-modal-close]"),
 
     modal: document.querySelector("[data-modal]"),
@@ -12,5 +12,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-open");
+    document.body.classList.toggle("modal-open");
   }
 })();
